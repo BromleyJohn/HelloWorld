6eb61f764e700ba65431c473ea91f069524d676c
BromleyJohn <john@fleetfacts.net>
2013-11-17 18:42:03 +0000

    init

--- www/js/HelloWorld.js
+++ www/js/HelloWorld.js
@@ -1,5 +1,8 @@
-  function ButtonClicked()
-   {
+function buttonClicked(){
       alert("You clicked the button!");
    }
 
+function changeColour(newColour) {
+    var elem = document.getElementById("para1");
+    elem.style.color = newColour;
+  }
