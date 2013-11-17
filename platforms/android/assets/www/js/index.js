c98e0f6f4700e5d599474b6c791f76e9b825bf0f
BromleyJohn <john@fleetfacts.net>
2013-11-17 22:47:20 +0000

    init

--- platforms/android/assets/www/js/index.js
+++ platforms/android/assets/www/js/index.js
@@ -20,7 +20,7 @@ var app = {
     // Application Constructor
     initialize: function() {
        
-       document.getElementById('loaded').innerHTML = 'LOADED';
+       document.getElementById('loaded').innerText = 'LOADED';
        this.bindEvents();
     },
     // Bind Event Listeners
