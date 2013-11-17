c9305a324d6cecefb60a47d9abe70c8af1dbd9b4
BromleyJohn <john@fleetfacts.net>
2013-11-17 20:56:39 +0000

    init

--- platforms/android/assets/www/js/HelloWorld.js
+++ platforms/android/assets/www/js/HelloWorld.js
@@ -6,3 +6,25 @@ function changeColour(newColour) {
     var elem = document.getElementById("para1");
     elem.style.color = newColour;
   }
+
+ 
+ $(document).ready(function(){
+    alert("U R here!!!"); 
+
+   // $.ajax({ // ajax call starts
+        
+   //      url: 'http://johnah.byethost7.com/cities.php?callback=?', // JQuery loads serverside.php
+        
+        //url: 'cities.php', // JQuery loads serverside.php
+  //	      dataType: 'jsonp', // Choosing a JSON datatype json for local jasonp for crossweb - NOT SURE ABOUT THIS!!!
+   //       success: function(data) // Variable data contains the data we get from serverside
+   //       {
+   //       $('#cities').html(''); // Clear #bands div
+   //       for (var i in data) {
+   //          $('#cities').append(data[i] + '<br/>');
+   //       }
+
+    //    }
+  //  });
+   // //return false; 
+});
